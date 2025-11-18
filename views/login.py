cbea5b156fd4de7104460cd67fcfa46529c32f63
Amarma <macua05mm@gmail.com>
2025-11-18 11:10:13 -0300

    Rediseño de interfaz

--- views/login.py
+++ views/login.py
@@ -32,24 +32,35 @@ def iniciar_login():  # 👈 Esta función es llamada desde main.py
         ventana.geometry("400x450")
         ventana.config(bg="#507383")
 
-        tk.Label(ventana, text="Nombre completo:", bg="#B3E5FC").pack(pady=5)
-        entry_nombre = tk.Entry(ventana)
+        frame_ventana_usuario = tk.Frame(ventana, bg="#507383", width=500) 
+        frame_ventana_usuario.pack(side="left", fill="both", padx=15, pady=15)
+
+        lbl_nuevo_usuario = tk.Label(
+        frame_ventana_usuario, # Empaquetado en el frame izquierdo
+        text=f"Cree su usuario 🐶",
+        font=("cambria", 25, "bold"),
+        bg="#507383",
+        )
+        lbl_nuevo_usuario.pack(pady=15)
+
+        tk.Label(frame_ventana_usuario, text="Nombre completo:", bg="#507383").pack(pady=5)
+        entry_nombre = tk.Entry(frame_ventana_usuario)
         entry_nombre.pack()
 
-        tk.Label(ventana, text="Fecha de nacimiento (DD/MM/AAAA):", bg="#B3E5FC").pack(pady=5)
-        entry_fecha = tk.Entry(ventana)
+        tk.Label(frame_ventana_usuario, text="Fecha de nacimiento (DD/MM/AAAA):", bg="#507383").pack(pady=5)
+        entry_fecha = tk.Entry(frame_ventana_usuario)
         entry_fecha.pack()
 
-        tk.Label(ventana, text="DNI:", bg="#B3E5FC").pack(pady=5)
-        entry_dni_nuevo = tk.Entry(ventana)
+        tk.Label(frame_ventana_usuario, text="DNI:", bg="#507383").pack(pady=5)
+        entry_dni_nuevo = tk.Entry(frame_ventana_usuario)
         entry_dni_nuevo.pack()
 
-        tk.Label(ventana, text="Email:", bg="#B3E5FC").pack(pady=5)
-        entry_email = tk.Entry(ventana)
+        tk.Label(frame_ventana_usuario, text="Email:", bg="#507383").pack(pady=5)
+        entry_email = tk.Entry(frame_ventana_usuario)
         entry_email.pack()
 
-        tk.Label(ventana, text="Teléfono:", bg="#B3E5FC").pack(pady=5)
-        entry_tel = tk.Entry(ventana)
+        tk.Label(frame_ventana_usuario, text="Teléfono:", bg="#507383").pack(pady=5)
+        entry_tel = tk.Entry(frame_ventana_usuario)
         entry_tel.pack()
 
         def guardar_usuario():
@@ -75,7 +86,7 @@ def iniciar_login():  # 👈 Esta función es llamada desde main.py
             ventana.destroy()
 
         tk.Button(
-            ventana, text="Guardar usuario", bg="#B3E5FC", fg="grey",
+            frame_ventana_usuario, text="Guardar usuario", bg="#B3E5FC", fg="grey",
             font=("Aptos", 11, "bold"), relief="raised",
             command=guardar_usuario
         ).pack(pady=20)
@@ -86,31 +97,31 @@ def iniciar_login():  # 👈 Esta función es llamada desde main.py
     root.geometry("400x500")
     root.config(bg="#507383")
 
-    frame = tk.Frame(root, bg="#B3E5FC")
+    frame = tk.Frame(root, bg="#507383")
     frame.pack(expand=True)
 
-    ruta_logo = os.path.join(os.path.dirname(__file__), "imagen", "logo3.png")
+    ruta_logo = os.path.join(os.path.dirname(__file__), "imagen", "logo1.png")
     try:
         logo_img = Image.open(ruta_logo).convert("RGBA")
-        logo_img = logo_img.resize((380, 280), Image.LANCZOS)
+        logo_img = logo_img.resize((380, 380), Image.LANCZOS)
         logo_tk = ImageTk.PhotoImage(logo_img)
-        lbl_logo = tk.Label(frame, image=logo_tk, bg="#B3E5FC")
+        lbl_logo = tk.Label(frame, image=logo_tk, bg="#507383")
         lbl_logo.image = logo_tk
         lbl_logo.pack(pady=(0, 0))
     except:
-        tk.Label(frame, text="🐾 SystemPet 🐾", bg="#B3E5FC",
+        tk.Label(frame, text="🐾 SystemPet 🐾", bg="#507383",
                  font=("Aptos Black", 18, "bold")).pack(pady=10)
 
-    tk.Label(frame, text="Ingrese su DNI:", bg="#B3E5FC",
+    tk.Label(frame, text="Ingrese su DNI:", bg="#507383",
              font=("Aptos", 14, "bold")).pack(pady=(2, 0))
-    entry_dni = tk.Entry(frame, font=("Arial", 14), width=20)
+    entry_dni = tk.Entry(frame, font=("cambria", 12), width=20)
     entry_dni.pack(pady=5)
 
-    tk.Button(frame, text="Ingresar", bg="#C8F0F5", fg="grey",
+    tk.Button(frame, text="Ingresar", bg="#D6DBDB", fg="black",
               font=("Aptos Bold", 11, "bold"), relief="raised",
               command=verificar_login).pack(pady=5)
 
-    tk.Button(frame, text="Crear usuario", bg="#C8F0F5", fg="grey",
+    tk.Button(frame, text="Crear usuario", bg="#D6DBDB", fg="black",
               font=("Aptos", 11, "bold"), relief="raised",
               command=crear_usuario_ui).pack(pady=5)
 
